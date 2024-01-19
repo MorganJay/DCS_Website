0b582c38793a2bb9cf55357004e44cd793a51ae1
MorganJay <jetmorgan.jm@gmail.com>
2024-01-19 23:50:10 +0000

    adding a new nav section for sm links and contact info

--- src/assets/website-data.ts
+++ src/assets/website-data.ts
@@ -1,1 +1,57 @@
 export const ENTITY_NAME = 'DCS Learning and Resource Centre';
+
+export const ENTITY_PHONE = '07961184890';
+
+export const ENTITY_ADDRESS = 'London Road, Purfleet RM19 1QT';
+export const ENTITY_EMAIL_ADDRESS = 'info@dcslearningcentre.org.uk';
+
+export const SOCIAL_ACCOUNTS_LIST = [
+  {
+    id: 1,
+    icon: 'fab fa-facebook',
+    link: 'https://www.facebook.com/',
+  },
+  {
+    id: 2,
+    icon: 'fab fa-twitter',
+    link: 'https://www.twitter.com/',
+  },
+  {
+    id: 3,
+    icon: 'fab fa-instagram',
+    link: 'https://www.instagram.com/',
+  },
+];
+
+export const NAVBAR_ITEMS = [
+  {
+    id: 1,
+    name: '',
+    path: '',
+    icon: 'fas fa-home',
+  },
+  {
+    id: 2,
+    name: 'about us',
+    path: 'about',
+    icon: 'fas fa-address-card',
+  },
+  {
+    id: 3,
+    name: 'services',
+    path: 'services',
+    icon: 'fas fa-blog',
+  },
+  {
+    id: 4,
+    name: 'activities and events',
+    path: 'activities',
+    icon: 'fas fa-book-open',
+  },
+  {
+    id: 5,
+    name: 'contact us',
+    path: 'contact',
+    icon: 'fas fa-phone-alt',
+  },
+];
--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -1,7 +1,13 @@
 import Link from 'next/link';
 
-import { accounts } from './Data';
-import { navbar_items } from '../navbar/Data';
+import {
+  ENTITY_PHONE,
+  ENTITY_ADDRESS,
+  ENTITY_EMAIL_ADDRESS,
+  ENTITY_NAME,
+  SOCIAL_ACCOUNTS_LIST,
+  NAVBAR_ITEMS,
+} from '@/assets/website-data';
 
 import './Footer.css';
 
@@ -12,10 +18,10 @@ const Footer = () => {
     <footer className="footer py-3 pb-4">
       <div className="container">
         <div className="footer-top py-3 mb-4 d-flex justify-content-center">
-          {accounts.map((item, index) => (
+          {SOCIAL_ACCOUNTS_LIST.map((item, index) => (
             <div
               className={`a account-${index} mx-2 px-2 py-1 rounded-circle shadow-lg`}
-              key={Math.random()}
+              key={item.id}
             >
               <a
                 className="text-center"
@@ -31,27 +37,25 @@ const Footer = () => {
         <div className="row mb-4">
           <div className="c col-lg-4 col-md-6 col-12">
             <div className="s mb-3 mb-md-0">
-              <h6 className="footer-title mb-3">
-                DCS Learning and Resource Centre
-              </h6>
+              <h6 className="footer-title mb-3">{ENTITY_NAME}</h6>
               <p className="footer-text">
                 Making a Difference, Building Community & Working Together to
                 Create a Better Future
               </p>
-              <a href="tel:07961974878">
+              <a href={`tel:${ENTITY_PHONE}`}>
                 <p className="footer-text">
-                  <i className="fas fa-phone-alt me-2"></i>Tel: 07961184890
+                  <i className="fas fa-phone-alt me-2"></i>Tel: {ENTITY_PHONE}
                 </p>
               </a>
               <p className="footer-text">
-                <i className="fas fa-map-marker-alt me-2"></i> Address: London
-                Road, Purfleet RM19 1QT
+                <i className="fas fa-map-marker-alt me-2"></i> Address:{' '}
+                {ENTITY_ADDRESS}
               </p>
 
-              <a href="mailto:info@dcslearningcentre.org.uk">
+              <a href={`mailto:${ENTITY_EMAIL_ADDRESS}`}>
                 <h6 className="footer-e-mail text-muted">
                   <i className="fas fa-envelope me-2"></i>
-                  info@dcslearningcentre.org.uk
+                  {ENTITY_EMAIL_ADDRESS}
                 </h6>
               </a>
             </div>
@@ -60,11 +64,11 @@ const Footer = () => {
             <div className="s mb-3 mb-md-0 text-center">
               <h6 className="link text-capitalize mb-3">quick links</h6>
               <div className="links">
-                {navbar_items.map(item => (
+                {NAVBAR_ITEMS.map(item => (
                   <Link
                     className="d-block mb-2 text-muted text-capitalize"
                     href={`/${item.path}`}
-                    key={Math.random()}
+                    key={item.id}
                   >
                     {item.name === '' ? 'home' : item.name}
                   </Link>
@@ -91,8 +95,7 @@ const Footer = () => {
         </div>
         <div className="copyrights text-center">
           <p className="small text-muted mb-0">
-            &copy; 2024 All rights reserved.{' '}
-            <Link href="/">DCS Learning and Resource Centre</Link>
+            &copy; 2024 All rights reserved. <Link href="/">{ENTITY_NAME}</Link>
           </p>
         </div>
       </div>
